f8a6a9f0c7b642b4cec8490508453194223cfc18
rootinj <149873967+rootinj@users.noreply.github.com>
2026-01-15 01:46:34 +0530

    Add files via upload

---
+++ learn.py
@@ -0,0 +1,36 @@
+import random
+
+letters = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+    'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+    'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+    'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+    'Y', 'Z'
+]
+
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+print("Welcome to Password Genrator")
+nr_letters = int(input("How many letters would you like in your password \n"))
+nr_symbol = int(input("How many symbol would you like in your password\n"))
+nr_numbers = int(input(f"How many numbers would you like ?\n"))
+password = ""
+for char in range(1, nr_letters + 1):
+    random_char=random.choice(letters)
+    print(random_char)
+    password+=random_char
+for char in range(1, nr_symbol + 1):
+    random_symbol=random.choice(symbols)
+    print(random_symbol)
+    password+=random_symbol
+for char in range(1, nr_numbers + 1):
+    random_number=random.choice(numbers)
+    print(random_number)
+    password+=random_number
+print(password)
+password_list = list(password)
+random.shuffle(password_list)
+final_password = ''.join(password_list)
+print(f"Your password is {final_password}")
